9c35e3da3b412fb23d41fdf3901c23ca89349c8e
irayspace <irayspacii@gmail.com>
2025-11-22 22:39:52 +0800

    feat: learn-decode-string

---
+++ 16-learn-decode-string/Main.java
@@ -0,0 +1,71 @@
+import java.util.ArrayDeque;
+
+class Main {
+
+    void main() {
+        final String firstCase = decodeString("3[a]2[bc]");
+        System.out.println("firstCase: " + firstCase.equals("aaabcbc"));
+
+        final String secondCase = decodeString("3[a2[c]]");
+        System.out.println("secondCase: " + secondCase.equals("accaccacc"));
+
+        final String thirdCase = decodeString("2[abc]3[cd]ef");
+        System.out.println("thirdCase: " + thirdCase.equals("abcabccdcdcdef"));
+    }
+
+    String decodeString(String s) {
+        StringBuilder decodedString = new StringBuilder();
+
+        final ArrayDeque<Integer> stack = new ArrayDeque<>();
+        final ArrayDeque<StringBuilder> stackSb = new ArrayDeque<>();
+
+        int idx = 0;
+        while (idx < s.length()) {
+
+            // Getting the characters
+            while (idx < s.length() && Character.isLowerCase(s.charAt(idx))) {
+                decodedString.append(s.charAt(idx));
+                idx = idx + 1;
+            }
+
+            if (idx < s.length() && Character.isDigit(s.charAt(idx))) {
+                final StringBuilder digits = new StringBuilder();
+                while (idx < s.length() && Character.isDigit(s.charAt(idx))) {
+                    digits.append(s.charAt(idx));
+                    idx = idx + 1;
+                }
+
+                if (s.charAt(idx) != '[') {
+                    throw new IllegalArgumentException(
+                            "Expected '[' at position " + idx + " but got '" + s.charAt(idx) + "'");
+                }
+
+                final int multiplier = Integer.parseInt(digits.toString());
+                stack.push(multiplier);
+                stackSb.push(decodedString);
+
+                decodedString = new StringBuilder();
+
+                idx = idx + 1;
+                continue;
+            }
+
+            if (idx < s.length() && s.charAt(idx) == ']') {
+                final int multiplier = stack.pop();
+
+                final StringBuilder multiplied = new StringBuilder();
+                for (int c = 0; c < multiplier; c++) {
+                    multiplied.append(decodedString);
+                }
+                
+                decodedString = stackSb.pop();
+                decodedString.append(multiplied);
+            }
+
+            idx = idx + 1;
+        }
+
+        return decodedString.toString();
+    }
+
+}
